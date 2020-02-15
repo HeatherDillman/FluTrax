d60142f368f8b6174069d80d3b00bd8e466c98bd
Fu Tianwen <kevin.futianwen@gmail.com>
2020-02-15 12:23:25 -0500

    more data

--- create_dummy_data.py
+++ create_dummy_data.py
@@ -3,7 +3,7 @@ from random import randint
 
 if __name__ == '__main__':
     data = []
-    dates = ['2/10/2020', '2/11/2020', '2/12/2020', '2/13/2020', '2/14/2020', '2/15/2020']
+    dates = [f'2/{day}/2020' for day in range(1, 15)]
     states = list(pd.read_csv('us_states.csv')['Abbreviation'].to_numpy())
     for state in states:
         pop = randint(15000, 25000)
